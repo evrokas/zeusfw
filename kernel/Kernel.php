e08b363c0b786c8508eecf94ca73be098626d388
Evangelos Rokas <evrokas@gmail.com>
2024-02-16 01:11:01 +0200

    progress on the framework for rendering the whole page

--- kernel/Kernel.php
+++ kernel/Kernel.php
@@ -8,11 +8,13 @@ function kernel_debug($dbg) {
 
 class Kernel {
     private static $rootpath = null;
-    private static $info = null;
+    private static $config = null;
+
+    private static $modules = array();
 
     function __construct($asrv, $ainfo) {
         self::$rootpath= substr($asrv['PHP_SELF'],0,strrpos($asrv['PHP_SELF'],'/')+1);
-        self::$info = $ainfo;
+        self::$config = $ainfo;
     }
 
     static function getrootpath() {
@@ -25,9 +27,34 @@ class Kernel {
       return ( self::$rootpath . $apath);
     }
 
-    static function getBlocks($aregion) {
-        $blks = self::$info['structure'];
+    static function getConfig() {
+        return (self::$config);
+    }
+
+    static function getRoutes() {
+        return (self::$config['routes']);
+    }
+
+    static function getBlocksInRegion($aregion) {
+        $blks = self::$config['structure'];
         // print_r( $blks[$aregion] );
       return ($blks[$aregion]);
+    }
+
+    static function registerModule($amod) {
+        echo "kernel: registering new module " . $amod->getName() . "<br/>";
+        self::$modules[$amod->getName()] = $amod;
+    }
+
+    static function getModule($amodulename) {
+        echo "requesting module with name " . $amodulename;
+        if(isset(self::$modules[ $amodulename ])) {
+            echo " Found!\n";
+            return( self::$modules[ $amodulename ] );
+        }
+        else {
+            echo " Not found!\n";
+            return null;
+        }
     }
 }
