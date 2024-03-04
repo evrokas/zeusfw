9c6ce9f5bf698e84e9ee25cd06104775d2115ccf
Evangelos Rokas <evrokas@gmail.com>
2024-03-04 11:38:12 +0200

    renamed UsersEx to ClassesEx

---
+++ lib/ClassesEx.php
@@ -0,0 +1,60 @@
+<?php
+
+// extend YAML generated classes to add some functionality
+
+class usersClassEx extends usersClass {
+    static function getUser( $uname, $upass ) {
+
+        global $AppDBConnection;
+
+        if(!$AppDBConnection->isConnected()) {
+            if(!$AppDBConnection->Connect()) {
+                echo 'Could not connect to database';
+                return (null);
+            }
+        }
+
+        $sql = "SELECT * FROM users WHERE uname=:uname AND upass=:upass";
+        $st = $AppDBConnection->getConnection()->prepare( $sql );
+        $st->bindValue(":uname", $uname, PDO::PARAM_STR);
+        $st->bindValue(":upass", $upass, PDO::PARAM_STR);
+        $st->execute();
+        $row = $st->fetch();
+
+        if($row) {
+            $rclass = new usersClass( "users");
+            $rclass->loadFields( $row );
+            return $rclass;
+        } else return (null);
+    }
+}
+
+// class patientsClassEx extends patientsClass {
+// }
+
+class appointmentsClassEx extends appointmentsClass {
+    static function getAppointmentsForPatient($pguid) {
+        global $AppDBConnection;
+
+        if(!$AppDBConnection->isConnected()) {
+            if(!$AppDBConnection->Connect()) {
+                echo 'Could not connect to database';
+                return (null);
+            }
+        }
+
+        $sql = "SELECT * FROM appointments WHERE pguid=:pguid ORDER BY adate";
+        $st = $AppDBConnection->getConnection()->prepare( $sql );
+        $st->bindValue(":pguid", $pguid, PDO::PARAM_STR);
+        $st->execute();
+
+        $list = array();        
+        while( $row = $st->fetch() ) {
+            $rclass = new appointmentsClass( "appointments" );
+            $rclass->loadFields( $row );
+            $list[] = $rclass;
+        }
+
+        return ($list);
+    }
+}
