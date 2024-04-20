0eb908bf8767039316cfa59b8338ff947adb26b4
Evangelos Rokas <evrokas@gmail.com>
2024-04-20 16:57:25 +0300

    implement basic content-type node rendering

--- lib/Content.php
+++ lib/Content.php
@@ -6,6 +6,7 @@ class ContentClass {
     private $filename;
     private $content;
     private $meta;
+    private $template;
     function __construct($afile) {
         global $kernel;
         // echo $afile;
@@ -16,17 +17,31 @@ class ContentClass {
         // echopre( print_r($str, 1) );
         
         $this->meta = yaml_parse( $str[0] );
-        echopre( print_r($this->meta, 1));
+        echopre( print_r($this->meta, 1) );
 
         if($str[1]) {
             $this->filename = $afile;
             $this->content = $str[1];
         }
+
+        /*
+         * {{guid}}.zetem
+         * basic-{name}-{viewmode}.zetem
+         * basic-{name}.zetem
+         * basic-{viewmode}.zetem
+         * basic.zetem
+         * 
+         */
+        // 
+        $this->template = $this->meta['contenttype'] . '.zetem';
     }
 
     function render() {
-        return $this->content;
-        // echo $conten
+        global $Renderer;
+
+
+        return $Renderer->render($this->template, $this->meta);
+        // return $this->content;
     }
 }
 
