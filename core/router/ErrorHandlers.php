77fb8f5d616846bfe80564928a3b6dfa8dd681ea
Evangelos Rokas <evrokas@gmail.com>
2025-03-09 22:22:23 +0200

    add message in error functions for html

--- core/router/ErrorHandlers.php
+++ core/router/ErrorHandlers.php
@@ -3,11 +3,11 @@
 /* common HTTP error handlers */
 
 // 404
-function error_404() {
-    return(Renderer::render('404.zetem', []));
+function error_404(string $errmsg = null): string {
+    return(Renderer::render('404.zetem', ['error' => $errmsg]));
 }
 
 
-function error_401() {
-    return(Renderer::render('401.zetem', []));
+function error_401(string $errmsg = null): string {
+    return(Renderer::render('401.zetem', ['error' => $errmsg]));
 }
