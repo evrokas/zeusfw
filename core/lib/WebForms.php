2fdae2e86e78d99ac77cbe3dd30eef9625a17661
Evangelos Rokas <evrokas@gmail.com>
2025-10-21 08:55:30 +0300

    progress is processing submitting forms, on rendering form and setup  FormActionHandler

--- core/lib/WebForms.php
+++ core/lib/WebForms.php
@@ -10,6 +10,7 @@ class formsClass {
         $dbForm = webFormsClass::sgetAllFilter('webforms', ['form_name' => $formname]);
         // echopre("Searching for form `$formname`");
         // echopre(print_r($dbForm, 1));
+        if(!$dbForm)return null;
 
         return ($dbForm[0]);
     }
@@ -64,11 +65,11 @@ class formsClass {
         $classData = new $formClass( $results );
         // echopre(print_r($classData, 1));
         
-        $classData->setguid( guid() );
-        $classData->setcdate( getDBtime() );
+        // $classData->setguid( guid() );
+        // $classData->setcdate( getDBtime() );
         
-        global $kernel;
-        $classData->setcuser( $kernel->getUserName() );
+        // global $kernel;
+        // $classData->setcuser( $kernel->getUserName() );
 
         $classData->insert();
     }
@@ -86,12 +87,43 @@ class formsClass {
             
             echopre(print_r($_SESSION, 1));
 
-            // $_POST['cuser'] = $_SESSION['']
+            global $kernel;
+            $_POST['cuser'] = $kernel->getUserName();
+            $_POST['pdob'] = getDBtime();
 
-            self::storeFormResults($form, $_POST);
+            if(isset($_SESSION['handler'])) {
+                $form_handler = $_SESSION['handler'];
+                echopre("form_handler: " . print_r($form_handler, 1));
+                $guid = $form_handler['guid'];
+                $handler = $form_handler['handler'];
+                echopre("form $guid handler $handler");
+
+
+                
+                if($handler) {
+                    if(function_exists( $handler )) {
+                        $result = $handler($params);
+                        if($result) {
+                            // handler returned succesfully
+                            unset( $_SESSION['handler'] );
+                            return $result;
+                        } else {
+                            return "ERROR: form handler `$handler` failed to execute";
+                        }
+                    } else {
+                        return "ERROR: form handler `$handler` could not be found";
+                    }
+                } else {
+                    return "ERROR: no form handler is defined";
+                }
+            }
+            // self::storeFormResults($form, $_POST);
 
             // echopre("session:" . print_r($_SESSION, 1));
+
+
         }
+
 
         return "Form: {$params['guid']}";
     }
@@ -103,7 +135,7 @@ class formsClass {
     }
 
 
-    static function renderFormResults($formname) {
+    static function renderFormResults($formname, $filter_fields = array()) {
         $form = self::getForm($formname);
 
         $formArray = json_decode($form->getdata(), true);
@@ -118,8 +150,36 @@ class formsClass {
         // unset($formArray['buttons']);
 
         $output = [$formClass . ' table results'];
+        if(!count($results))return $output;
+        
         $html = [];
         $formArray['inputs_list'] = [];
+
+        $input_row = [];
+        $out = [];
+
+        // get fields from first result
+        $fields = $results[0]->getFields();
+
+        $out[] = "<thead>";
+        // setup headers
+        foreach($formArray['inputs'] as $key => $input) {
+            // echopre("Input: " . $input['name']);
+            // echopre("Filter field: " . print_r(array_values($filter_fields), 1));
+            // echopre("in filter: " . in_array($input['name'], array_values($filter_fields)));
+            if(key_exists($input['name'], $fields) &&
+            (empty($filter_fields) || (!empty($filter_fields) && in_array($input['name'], array_values($filter_fields))))) {
+                // $out[] = "{{ " . $fields[ $input['name' ] ] . " }} ";
+                $out[] = "<td> " . $input['name' ] . "</td>";
+
+                // $formArray['inputs'][$key]['default'] = $fields[ $input['name'] ];
+                // $formArray['inputs'][$key]['attributes']['disabled'] = "disabled";
+                
+                // $input_row[] = $formArray['inputs'][$key];
+            }
+        }
+        $out[] = "</thead>";
+        $output[] = implode(',', $out);
 
         foreach($results as $entry) {
             $out = [];
@@ -128,18 +188,21 @@ class formsClass {
             // echopre("Fields: " . print_r($fields, 1));
             foreach($formArray['inputs'] as $key => $input) {
                 // echopre("Input: " . $input['name']);
-                if(key_exists($input['name'], $fields))
+                // echopre("Filter field: " . print_r(array_values($filter_fields), 1));
+                // echopre("in filter: " . in_array($input['name'], array_values($filter_fields)));
+                if(key_exists($input['name'], $fields) &&
+                (empty($filter_fields) || (!empty($filter_fields) && in_array($input['name'], array_values($filter_fields))))) {
                     $out[] = "{{ " . $fields[ $input['name' ] ] . " }} ";
-                $formArray['inputs'][$key]['default'] = $fields[ $input['name'] ];
-                $formArray['inputs'][$key]['attributes']['disabled'] = "disabled";
 
-                $input_row[] = $formArray['inputs'][$key];
-                
+                    $formArray['inputs'][$key]['default'] = $fields[ $input['name'] ];
+                    $formArray['inputs'][$key]['attributes']['disabled'] = "disabled";
+                    
+                    $input_row[] = $formArray['inputs'][$key];
+                }
             }
             $formArray['inputs_list'][] = $input_row;
 
-            $output[] = implode(',', $out);
-            
+            // $output[] = implode(',', $out);
         }
         // echopre("input_lists: " . print_r($formArray, 1));
         $html[] = generateHTMLFormTable($formArray);
@@ -152,4 +215,19 @@ class formsClass {
 
         return $output;
     }
+}
+
+
+function setupFormActionHandler($form, $handler) {
+    $guid = formsClass::getForm($form['name'])->getguid();
+    $handler = [
+        'handler' => $handler,
+        'time' => time(),
+        'guid' => $guid
+    ];
+
+    $_SESSION['handler'] = $handler;
+    // echopre("setupFormActionHandler: " . print_r($handler, 1));
+    // $handlerElement = new HiddenElement($form['name'], ['type' => 'hidden', 'name' => 'handler', 'value' => $handler] );
+    // $formArray['handler'] = $handler;
 }
